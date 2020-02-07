214c32c1c1d19539dafed311513be3a952934311
Ronnie Short <ronnieshort252@gmail.com>
2020-02-07 15:28:06 -0500

    SoundBite+
    The sound has been up graded

---
+++ app/src/main/java/com/example/nut/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.nut
+
+import android.media.MediaPlayer
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val nutButton: ImageButton = findViewById(R.id.sayNutButton)
+
+        nutButton.setOnClickListener {
+            var nutSayer = MediaPlayer.create(this, R.raw.male_nut)
+            nutSayer.start()
+        }
+    }
+}
